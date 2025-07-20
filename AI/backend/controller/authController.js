f49348166b80d9954c0472fc78a11e2f760f8d75
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-07-20 21:25:55 +0530

    Update authController.js

--- AI/backend/controller/authController.js
+++ AI/backend/controller/authController.js
@@ -95,11 +95,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token", {
-            httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
-        });
+    res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,       
+  sameSite: "None",   
+});
+
 
         res.status(200).json({ message: "Logout successful" });
     } catch (error) {
