4ec1b1258d0bede6c1245b183d199df4ea3b3889
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-07-20 21:34:50 +0530

    Update AuthContext.jsx

--- AI/admin/src/context/AuthContext.jsx
+++ AI/admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from "react";
 
 export const authDataContext = createContext();
 
-const serverUrl = "http://localhost:8000";
+const serverUrl =  "https://ai-backend-f9av.onrender.com"
 
 export function AuthContext({ children }) {
     const value = {
